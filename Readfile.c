e4b793ea097361ad0e570743c3462e2daf334d9e
Ricardo Rouco <ricardo.rouco@gmail.com>
2022-06-29 21:21:43 +0100

    Add files via upload

---
+++ Readfile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void) {
+    const char* filename = "input.txt";
+
+    FILE* input_file = fopen(filename, "r");
+    if (!input_file)
+        exit(EXIT_FAILURE);
+
+    char *contents = NULL;
+    size_t len = 0;
+    while (getline(&contents, &len, input_file) != -1){
+        printf("%s", contents);
+    }
+
+    fclose(input_file);
+    free(contents);
+
+    exit(EXIT_SUCCESS);
+}
